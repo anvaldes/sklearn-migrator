8efc975f3665ed1c294cadcd2b8f0f47712f3e81
Alberto <anvaldes@uc.cl>
2025-07-19 12:27:00 -0400

    Changes in Logistic Regression

--- sklearn_migrator/classification/logistic_regression_clf.py
+++ sklearn_migrator/classification/logistic_regression_clf.py
@@ -2,24 +2,27 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 
 def serialize_logistic_regression_clf(model, version_in):
-    serialized_model = {
+
+    metadata = {
         'meta': 'lr',
         'classes_': model.classes_.tolist(),
         'coef_': model.coef_.tolist(),
         'intercept_': model.intercept_.tolist(),
         'n_iter_': model.n_iter_.tolist(),
-        'params': model.get_params()
+        'params': model.get_params(),
+        'version_sklearn_in': version_in
     }
 
-    return serialized_model
+    return metadata
 
 
-def deserialize_logistic_regression_clf(model_dict, version_out):
-    model = LogisticRegression(model_dict['params'])
+def deserialize_logistic_regression_clf(data, version_out):
 
-    model.classes_ = np.array(model_dict['classes_'])
-    model.coef_ = np.array(model_dict['coef_'])
-    model.intercept_ = np.array(model_dict['intercept_'])
-    model.n_iter_ = np.array(model_dict['intercept_'])
+    model = LogisticRegression(data['params'])
+
+    model.classes_ = np.array(data['classes_'])
+    model.coef_ = np.array(data['coef_'])
+    model.intercept_ = np.array(data['intercept_'])
+    model.n_iter_ = np.array(data['intercept_'])
 
     return model
